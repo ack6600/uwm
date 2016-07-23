f708834e058c697702d456aa4c5f2b13aa7e345c
Adam Urish <adamurish@gmail.com>
2016-07-23 14:35:53 -0500

    real code stuff

--- src/uwm.c
+++ src/uwm.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <xcb/xcb.h>
 
 int main(){
-	xcb_connection_t *connection = xcb_connect(NULL, NULL);
+	int i, screen_num;
+	xcb_connection_t *connection = xcb_connect(NULL, &screen_num);
+	const xcb_setup_t *setup = xcb_get_setup(connection);
+	xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
+	for(i = 0; i < screen_num; ++i){
+		xcb_screen_next(&iter);
+	}
+	xcb_screen_t *screen = iter.data;
+	xcb_window_t window = xcb_generate_id(connection);
+	xcb_create_window(connection,
+			XCB_COPY_FROM_PARENT,
+			window,
+			screen->root,
+			0,
+			0,
+			150,
+			150,
+			0,
+			XCB_WINDOW_CLASS_INPUT_OUTPUT,
+			screen->root_visual,
+			0, NULL);
+	xcb_map_window(connection,window);
+	xcb_flush(connection);
+	pause();
+	xcb_disconnect(connection);
+	return 0;
 }
